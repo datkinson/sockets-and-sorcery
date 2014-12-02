c8c6675bd7d8ba9ea63161a0b66e7a7bd44025eb
Daniel Atkinson <hourd.tasa@gmail.com>
2014-12-02 16:21:45 +0000

    adjusted style application to allow user to define size unit type (default sto px)

--- public/dynamic.js
+++ public/dynamic.js
@@ -4,11 +4,27 @@ window.onload = function() {
     var frame = document.getElementById("frame");
 
     socket.on('message', function (data) {
+        if(typeof(data.width) !== 'undefined' && data.width.indexOf('px') > -1){
+            data.width = data.width + 'px';
+            jQuery('#frame').css('width', data.width);
+        }
+
+        if(typeof(data.height) !== 'undefined' && data.height.indexOf('px') > -1){
+            data.height = data.height + 'px';
+            jQuery('#frame').css('height', data.height);
+        }
+
+        if(typeof(data.left) !== 'undefined' && data.left.indexOf('px') > -1){
+            data.left = data.left + 'px';
+            jQuery('#frame').css('left', data.left);
+        }
+
+        if(typeof(data.top) !== 'undefined' && data.top.indexOf('px') > -1){
+            data.top = data.top + 'px';
+            jQuery('#frame').css('top', data.top);
+        }
+
         jQuery('#frame').attr('src', data.url);
-        jQuery('#frame').css('width', data.width + 'px');
-        jQuery('#frame').css('height', data.height + 'px');
-        jQuery('#frame').css('left', data.left + 'px');
-        jQuery('#frame').css('top', data.top + 'px');
     });
 
 
