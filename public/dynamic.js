9acca77840d6035b0aad3c4550e9163e6f8d0b2f
Daniel Atkinson <hourd.tasa@gmail.com>
2014-12-13 15:49:42 +0000

    Adjusted dynamic page to ba able to use % px or neight, defaulting to px

--- public/dynamic.js
+++ public/dynamic.js
@@ -4,24 +4,33 @@ window.onload = function() {
     var frame = document.getElementById("frame");
 
     socket.on('message', function (data) {
-        if(typeof(data.width) !== 'undefined' && data.width.indexOf('px') > -1){
-            data.width = data.width + 'px';
+        
+	if(typeof(data.width) !== 'undefined') {
+	    if(data.width.indexOf('px') == -1 && data.width.indexOf('%') == -1){
+                data.width = data.width + 'px';
+	    }
             jQuery('#frame').css('width', data.width);
         }
 
-        if(typeof(data.height) !== 'undefined' && data.height.indexOf('px') > -1){
-            data.height = data.height + 'px';
+        if(typeof(data.height) !== 'undefined') {
+	    if(data.height.indexOf('px') == -1 && data.height.indexOf('%') == -1){
+                data.height = data.height + 'px';
+	    }
             jQuery('#frame').css('height', data.height);
         }
 
-        if(typeof(data.left) !== 'undefined' && data.left.indexOf('px') > -1){
-            data.left = data.left + 'px';
-            jQuery('#frame').css('left', data.left);
+        if(typeof(data.top) !== 'undefined') {
+	    if(data.top.indexOf('px') == -1 && data.top.indexOf('%') == -1){
+                data.top = data.top + 'px';
+	    }
+            jQuery('#frame').css('top', data.top);
         }
 
-        if(typeof(data.top) !== 'undefined' && data.top.indexOf('px') > -1){
-            data.top = data.top + 'px';
-            jQuery('#frame').css('top', data.top);
+        if(typeof(data.left) !== 'undefined') {
+	    if(data.left.indexOf('px') == -1 && data.left.indexOf('%') == -1){
+                data.left = data.left + 'px';
+	    }
+            jQuery('#frame').css('left', data.left);
         }
 
         jQuery('#frame').attr('src', data.url);
