b1dcf55dae197e9b64218ad8de8df032d2b66527
Dan Atkinson <hourd.tasa@gmail.com>
2014-12-21 20:09:18 +0000

    attempted to add multiple sprites and changed bg color

--- public/sorcerer.js
+++ public/sorcerer.js
@@ -2,10 +2,16 @@ var width = $(window).width();
 var height = $(window).height();
 
 // create an new instance of a pixi stage
-var stage = new PIXI.Stage(0x66FF99);
+var stage = new PIXI.Stage(0x6495ED);
 
+var myDisplayResolution = window.devicePixelRatio;
+ 
+// create an options object and include our resolution
+var renderOptions = {
+    resolution:myDisplayResolution
+};
 // create a renderer instance.
-var renderer = PIXI.autoDetectRenderer(width, height);
+var renderer = PIXI.autoDetectRenderer(width, height, renderOptions);
 
 // add the renderer view element to the DOM
 document.body.appendChild(renderer.view);
@@ -14,27 +20,52 @@ requestAnimFrame( animate );
 
 // create a texture from an image path
 var texture = PIXI.Texture.fromImage("/img/player.png");
+
+// create many players
+var players = [];
+
 // create a new Sprite using the texture
-var player = new PIXI.Sprite(texture);
-
-player.scale.x = 0.5;
-player.scale.y = 0.5;
-
-// center the sprites anchor point
-player.anchor.x = 0.5;
-player.anchor.y = 0.5;
-
-// move the sprite to the center of the screen
-player.position.x = width/2;
-player.position.y = height/2;
-
-stage.addChild(player);
+//var player = new PIXI.Sprite(texture);
+//
+//player.scale.x = 0.5;
+//player.scale.y = 0.5;
+//
+//// center the sprites anchor point
+//player.anchor.x = 0.5;
+//player.anchor.y = 0.5;
+//
+//// move the sprite to the center of the screen
+//player.position.x = width/2;
+//player.position.y = height/2;
+//
+//stage.addChild(player);
 
 function animate() {
 
     requestAnimFrame( animate );
 
-    player.rotation += 0.1;
+//    player.rotation += 0.1;
+    
+    players.forEach(function(item) {
+        item.rotation += 0.1;
+    });
 
     renderer.render(stage);
 }
+
+function generatePlayers(number) {
+    for (i = 0; i < number; i++) {
+        players[i] = new PIXI.Sprite(texture);
+        players[i].scale.x = 0.5;
+        players[i].scale.y = 0.5;
+        players[i].anchor.x = 0.5;
+        players[i].anchor.y = 0.5;
+        var x = ((width/number)*(i+1));
+        players[i].position.x = ((width/number)*(i+1)) - (players[i].width/2);
+        console.log('player '+i+' x', x);
+        console.log((players[i].width));
+        players[i].position.y = height/2;
+        stage.addChild(players[i]);
+    }
+}
+generatePlayers(3);
